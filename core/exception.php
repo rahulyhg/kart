db1514067fa452eb0bf28581e824bb529ccd764a
Paul Houser <paul@aja>
2015-03-10 10:47:18 -0400

    Cleaned out unstable code

--- core/exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Kart;
-
-class Exception extends \Exception 
-{
-
-}
--- kart.php
+++ kart.php
@@ -8,9 +8,5 @@ define("KART_DIR",dirname(__FILE__));
 
 require_once KART_DIR . "/core/events/events.php";
 
-require_once KART_DIR . "/core/cart.php";
-require_once KART_DIR . "/core/exception.php";
-require_once KART_DIR . "/core/order.php";
-
 //** load configuration, create if not available **//
 
